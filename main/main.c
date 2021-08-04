c54f1d114b0b6b35f1c9dd645151b4d01e0fcb7a
ipikin <illia.pikin@dgngames.com>
2021-08-04 18:41:18 +0300

    Updated sensor management

--- main/main.c
+++ main/main.c
@@ -16,44 +16,47 @@ static void bme280_task(void *prv)
     uint32_t press;
     uint32_t hum;
 
-    if (bme280_init(bme280_config))
+bme280_init:
+    while (!bme280_init(bme280_config))
     {
-        while (true)
-        {
-            if (!bme280_trigger_forced_read())
-            {
-                printf("bme280_trigger_forced_read failed!\r\n");
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-                continue;
-            }
-            vTaskDelay(20 / portTICK_PERIOD_MS);
-
-            if (!bme280_read_sensor_data())
-            {
-                printf("bme280_read_sensor_data failed!\r\n");
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-                continue;
-            }
-
-            temp = bme280_get_temperature();
-            printf("Temp: %d.%d DegC", (int)(temp / 100), (int)(temp % 100));
-            press = bme280_get_pressure();
-            printf(", Pres: %d.%d hPa", (int)(press / 100), (int)(press % 100));
-            if (bme280_is_humidity_supported())
-            {
-                hum = bme280_get_humidity();
-                printf(", Hum: %d.%d pct \r\n", (int)(hum / 1024), (int)(hum % 1024));
-            }
-            else
-            {
-                printf("\r\n");
-            }
-
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
+        bme280_dispose();
+        printf("bme280_init: failed! Next attempt in 5 sec...\r\n");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
-    bme280_dispose();
-    printf("bme280_task failed!\r\n");
+
+    while (true)
+    {
+        if (!bme280_trigger_forced_read())
+        {
+            printf("bme280_trigger_forced_read: failed!\r\n");
+            bme280_dispose();
+            goto bme280_init;
+        }
+        vTaskDelay(20 / portTICK_PERIOD_MS);
+
+        if (!bme280_read_sensor_data())
+        {
+            printf("bme280_read_sensor_data: failed!\r\n");
+            bme280_dispose();
+            goto bme280_init;
+        }
+
+        temp = bme280_get_temperature();
+        printf("Temp: %d.%d DegC", (int)(temp / 100), (int)(temp % 100));
+        press = bme280_get_pressure();
+        printf(", Pres: %d.%d hPa", (int)(press / 100), (int)(press % 100));
+        if (bme280_is_humidity_supported())
+        {
+            hum = bme280_get_humidity();
+            printf(", Hum: %d.%d pct \r\n", (int)(hum / 1024), (int)(hum % 1024));
+        }
+        else
+        {
+            printf("\r\n");
+        }
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
 void app_main()
